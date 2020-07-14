742018eac6aa4c1386b60e76076a26899648b208
Matthias Buehlmann <matthias.buehlmann@mabulous.com>
2020-07-14 19:32:02 +0200

    BVHN<4> is required in any case.

--- kernels/bvh/bvh.cpp
+++ kernels/bvh/bvh.cpp
@@ -196,8 +196,6 @@ namespace embree
   template class BVHN<8>;
 #endif
 
-#if !defined(__AVX__) || !defined(EMBREE_TARGET_SSE2) && !defined(EMBREE_TARGET_SSE42)
-  template class BVHN<4>;
-#endif
+template class BVHN<4>;
 }
 
